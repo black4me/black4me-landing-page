dfe2b7fcf2e7cab1614ab1331c8c68d17c3e3137
black4me <black4mestore@gmail.com>
2026-07-02 16:41:00 +0400

    fix: type error in scripts

--- scripts/create_public_bucket.ts
+++ scripts/create_public_bucket.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
-const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
 
 async function main() {
   const { data: buckets } = await supabaseAdmin.storage.listBuckets();
