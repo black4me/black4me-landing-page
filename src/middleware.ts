8e33f50d12b15c4325078b4ab44650f58c485a57
black4me <black4mestore@gmail.com>
2026-07-07 05:36:30 +0400

    fix: bypass middleware for cal-webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -151,11 +151,12 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api/cal-webhook (Cal.com Webhook)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api/cal-webhook|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
